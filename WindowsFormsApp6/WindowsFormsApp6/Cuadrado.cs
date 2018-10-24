e491a4c653b824a78f98998f4eec67a54dd5b3b5
erriveros <erriveros@miuandes.cl>
2018-10-24 12:27:23 -0300

    'O

---
+++ WindowsFormsApp6/WindowsFormsApp6/Cuadrado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp6
+{
+    public class Cuadrado
+    {
+        public string Color { get; set; }
+        public string Letra { get; set; }
+        public int Tamano { get; set; }
+        public List<List<int>> Posicion { get; set; }
+        
+    }
+
+}
